84f0c879cff27b42f71e2815c324da90cd256217
phlimadev <devphlima@gmail.com>
2025-02-08 15:10:50 -0300

    adding new point registration method

--- src/main/java/br/com/phlimadev/points_of_interest/services/PointsOfInterestService.java
+++ src/main/java/br/com/phlimadev/points_of_interest/services/PointsOfInterestService.java
@@ -1,13 +1,26 @@
 package br.com.phlimadev.points_of_interest.services;
 
+import br.com.phlimadev.points_of_interest.dtos.RegisterDTO;
+import br.com.phlimadev.points_of_interest.entities.PointsOfInterest;
 import br.com.phlimadev.points_of_interest.infra.exceptions.NegativeCoordinateException;
+import br.com.phlimadev.points_of_interest.repositories.PointsOfInterestRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PointsOfInterestService {
+    @Autowired
+    private PointsOfInterestRepository pointsOfInterestRepository;
+
     public void validate(Integer coordinateX, Integer coordinateY) {
         if (coordinateX < 0 || coordinateY < 0) {
             throw new NegativeCoordinateException("Both coordinates must be positive.");
         }
+    }
+
+    public void register(RegisterDTO register) {
+        validate(register.coordinateX(), register.coordinateY());
+        PointsOfInterest newPoint = new PointsOfInterest(register);
+        pointsOfInterestRepository.save(newPoint);
     }
 }
