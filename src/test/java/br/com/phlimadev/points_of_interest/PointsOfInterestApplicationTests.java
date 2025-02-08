4c24f3335a77ed59fdefdf91f540df06bd31949d
phlimadev <devphlima@gmail.com>
2025-02-08 14:34:56 -0300

    initial configuration

---
+++ src/test/java/br/com/phlimadev/points_of_interest/PointsOfInterestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.phlimadev.points_of_interest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PointsOfInterestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
