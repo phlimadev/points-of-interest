ac462a43b8f999be25ac72418cd9327df3cb5c61
phlimadev <devphlima@gmail.com>
2025-05-30 07:53:04 -0300

    feat: creating constructor responsible for converting entity into dto

--- src/main/java/br/com/phlimadev/points_of_interest/controller/dto/PointOfInterestDTO.java
+++ src/main/java/br/com/phlimadev/points_of_interest/controller/dto/PointOfInterestDTO.java
@@ -1,5 +1,6 @@
 package br.com.phlimadev.points_of_interest.controller.dto;
 
+import br.com.phlimadev.points_of_interest.domain.PointOfInterest;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
@@ -16,4 +17,7 @@ public record PointOfInterestDTO(
         @PositiveOrZero
         Integer coordinateY
 ) {
+        public PointOfInterestDTO(PointOfInterest entity) {
+                this(entity.getName(), entity.getCoordinateX(), entity.getCoordinateY());
+        }
 }
