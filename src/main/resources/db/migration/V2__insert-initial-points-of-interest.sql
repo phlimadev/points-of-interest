70a8e6ca1d915a1d3ad5504ce4254c09560ea75f
phlimadev <devphlima@gmail.com>
2025-05-30 08:13:00 -0300

    feat: creating migration that inserts the initial points of interest

---
+++ src/main/resources/db/migration/V2__insert-initial-points-of-interest.sql
@@ -0,0 +1,9 @@
+INSERT INTO points_of_interest (name, coordinate_x, coordinate_y)
+VALUES
+    ('Lanchonete', 27, 12),
+    ('Posto', 31, 18),
+    ('Joalheria', 15, 12),
+    ('Floricultura', 19, 21),
+    ('Pub', 12, 8),
+    ('Supermercado', 23, 6),
+    ('Churrascaria', 28, 2);
